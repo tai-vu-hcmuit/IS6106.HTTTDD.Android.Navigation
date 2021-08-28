ee2861ebc28cfb0214067133460d66dfb775aecd
vcttai <vcttai@gmail.com>
2021-08-28 16:34:08 +0700

    Add lib for navigation, add login fragment

---
+++ app/src/main/java/taivu/uit/htttdd/trynavigation/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package taivu.uit.htttdd.trynavigation.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
