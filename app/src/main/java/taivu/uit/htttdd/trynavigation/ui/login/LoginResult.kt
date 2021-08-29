6f2bc55b99f9188582b3505f246046de0310210b
vcttai <vcttai@gmail.com>
2021-08-29 10:46:04 +0700

    Delete default login fragment

--- app/src/main/java/taivu/uit/htttdd/trynavigation/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package taivu.uit.htttdd.trynavigation.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
